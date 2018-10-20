6eb47749a92fa1e98cfe875b213d2efdd7876662
msweeneydev <matthewsweeney@mailup.net>
2018-10-20 07:46:25 +0100

    remove Text

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
-import Text from './components/Text';
+import TodoList from './components/TodoList';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <Text
-          text={`To get started, edit <code>src/App.tsx</code> and save to reload.`}
-        />
+        <TodoList />
       </div>
     );
   }
---
+++ src/components/TodoList/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class TodoList extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <p>I am the todolist.</p>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
