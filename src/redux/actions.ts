eccb9dbebc6bc175dfbe29e969dfdbb7bea90f24
msweeneydev <matthewsweeney@mailup.net>
2018-10-20 08:03:13 +0100

    added further types/actions

--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -1,1 +1,21 @@
-import { ITodo, ITodoList } from './types';
+import {
+  actions,
+  addTodoAction,
+  deleteTodoAction,
+  getTodosAction,
+  ITodo
+} from './types';
+
+export const getTodos = (): getTodosAction => ({
+  type: actions.GET_TODOS
+});
+
+export const addTodo = (data: ITodo): addTodoAction => ({
+  type: actions.ADD_TODO,
+  data
+});
+
+export const deleteTodo = (id: string): deleteTodoAction => ({
+  type: actions.DELETE_TODO,
+  id
+});
--- src/redux/types.ts
+++ src/redux/types.ts
@@ -1,3 +1,23 @@
+export enum actions {
+  ADD_TODO = 'AddTodo',
+  DELETE_TODO = 'DeleteTodo',
+  GET_TODOS = 'GetTodos'
+}
+
+export interface addTodoAction {
+  type: actions.ADD_TODO;
+  data: ITodo;
+}
+
+export interface deleteTodoAction {
+  type: actions.DELETE_TODO;
+  id: string;
+}
+
+export interface getTodosAction {
+  type: actions.GET_TODOS;
+}
+
 export interface ITodo {
   [key: string]: string;
 }
